554a4935fd00348ed4af09db0dfc08a72a7874ab
Farhan Labib <jahin2000.11.15@gmail.com>
2026-01-04 16:36:50 -0800

    alignment

--- lib/main.dart
+++ lib/main.dart
@@ -29,114 +29,20 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter Demo Home Page'),
       ),
       body: 
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(right:6),
-                        height: 150,
-                        width: 110,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.purple, Colors.pink],
-                          ),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(62),
-                              topRight: Radius.circular(2),
-                              bottomLeft: Radius.circular(2),
-                              bottomRight: Radius.circular(62),
-                            ),
-                        ),
-                        child: const Text(""),
-                      ),
-
-                      Container(
-                        margin: const EdgeInsets.only(right:6),
-                        height: 150,
-                        width: 110,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.blue, Colors.cyan],
-                          ),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(62),
-                              topRight: Radius.circular(2),
-                              bottomLeft: Radius.circular(2),
-                              bottomRight: Radius.circular(62),
-                            ),
-                        ),
-                        child: const Text(""),
-                      ),
-
-                      Container(
-                        margin: const EdgeInsets.only(right:6),
-                        height: 150,
-                        width: 110,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.green, Colors.lightGreenAccent],
-                          ),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(62),
-                              topRight: Radius.circular(2),
-                              bottomLeft: Radius.circular(2),
-                              bottomRight: Radius.circular(62),
-                            ),
-                        ),
-                        child: const Text(""),
-                      ),
-
-                      Container(
-                        margin: const EdgeInsets.only(right:6),
-                        height: 150,
-                        width: 110,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.orange, Colors.deepOrange],
-                          ),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(62),
-                              topRight: Radius.circular(2),
-                              bottomLeft: Radius.circular(2),
-                              bottomRight: Radius.circular(62),
-                            ),
-                        ),
-                        child: const Text(""),
-                      ),
-                    ],
-                  ),
-                ),
-            
-                
-        
-                    
-            
-                Container(
-                  margin: const EdgeInsets.only(bottom: 11),
-                  height: 900,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [Colors.lightBlue, Colors.red],
-                            ),
-                            borderRadius: BorderRadius.circular(62)
-                          ),
-                  child: const Text(""),
-                      ),
-            
-                
-            
-               
-              ],
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(86.0),
+            margin: EdgeInsets.only(top: 220.0),
+            color: Colors.blue,
+            child: Text("Login")
+          
+          
             ),
-          ),
-        ),
+        ],
+      )
     );
   }
 }
